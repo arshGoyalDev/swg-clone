5bd9a772e636ff244010dabbc0b824adce0cc1bd
arshGoyalDev <arshgoyalDev@gmail.com>
2024-08-18 15:56:21 +0530

    add: header

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,18 @@
+import { type Metadata } from "next";
+
+const metadata: Metadata = {
+  title: "SWG | About",
+  description: "Student's Welfare Group (SWG) Home PAge",
+};
+
 const AboutPage = () => {
   return (
-    <div>About Page</div>
-  )
-}
-export default AboutPage
+    <main>
+      <div></div>
+      About Page
+    </main>
+  );
+};
+
+export { metadata };
+export default AboutPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
+import { type Metadata } from "next";
+import Header from "~/components/home/Header";
+
+const metadata: Metadata = {
+  title: "SWG",
+  description: "Student's Welfare Group (SWG) Home PAge",
+};
+
 
 const HomePage = () => {
   return (
-    <main>
-      Home Page
+    <main className="min-h-[300vh]">
+      <Header />
     </main>
   );
 }
 
+export {metadata}
 export default HomePage;
