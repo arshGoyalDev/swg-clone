211fc99560dd4ee388c96584e2246a4f84ebd95b
arshGoyalDev <arshgoyalDev@gmail.com>
2024-08-21 15:07:42 +0530

    style: team & resource card

--- src/components/cards/ResourceCard.tsx
+++ src/components/cards/ResourceCard.tsx
@@ -12,11 +12,11 @@ const ResourceCard = ({
   link: string;
 }) => {
   return (
-    <div className="relative h-[240px] w-[92vw] cursor-default overflow-hidden rounded-2xl transition-all hover:shadow-xl md:h-[300px] md:max-w-[500px]">
+    <div className="card--resource relative h-[240px] w-[92vw] cursor-default overflow-hidden rounded-2xl transition-all hover:shadow-xl md:h-[300px] md:max-w-[500px]">
       <img src={bgImage} alt={title} className="h-full w-full" />
       <div className="absolute left-0 top-0 z-[5] h-full w-full bg-black bg-opacity-10"></div>
       <Link href={link}>
-        <div className="absolute bottom-4 left-4 right-4 z-10 flex flex-col gap-1 rounded-xl bg-black bg-opacity-60 p-4 text-white backdrop-blur-lg transition-all hover:rounded-none hover:bottom-0 hover:left-0 hover:right-0">
+        <div className="card--resource--info absolute bottom-4 left-4 right-4 z-10 gird place-content-center gap-1 rounded-xl bg-black bg-opacity-40 p-4 text-white backdrop-blur-lg transition-all">
           <h2 className="text-lg">{title}</h2>
           <p className="text-gray-300">{content}</p>
         </div>
--- src/components/cards/TeamCard.tsx
+++ src/components/cards/TeamCard.tsx
@@ -8,17 +8,13 @@ const TeamCard = ({
   profilePic: string;
 }) => {
   return (
-    <div className="overflow-hidden rounded-xl border-4 border-gray-200">
-      <div className="grid place-content-center bg-zinc-900 px-10 py-8">
-        <div className="rounded-full border-4 border-white p-1">
-          <div className="team-pic-container h-32 w-32 rounded-full">
-            <img src={profilePic} alt={name} />
-          </div>
-        </div>
+    <div className="card--team relative">
+      <div className="h-[280px] w-[90vh] overflow-hidden rounded-xl md:max-w-[320px]">
+        <img src={profilePic} alt={name} />
       </div>
-
-      <div className="bg-zinc-600 py-6">
-        <p className="text-lg font-semibold text-white">{name}</p>
+      <div className="absolute left-0 top-0 h-full w-full rounded-xl bg-black bg-opacity-15"></div>
+      <div className="card--team--info absolute bottom-4 left-4 right-4 z-10 grid place-content-center gap-1 rounded-xl bg-black bg-opacity-10 p-4 text-white backdrop-blur-md transition-all">
+        <h2 className="text-lg">{name}</h2>
         {role && <p className="text-gray-300">{role}</p>}
       </div>
     </div>
